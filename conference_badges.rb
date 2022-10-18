3891e68cbe75724cb53297d176d2393cebf74446
+ <+>
2022-10-18 14:50:01 +0300

    commit tree 03b10184117beab69458dcd4eef275b7428cc10d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(messages)
+    return "Hello, #{messages}."
+end
+badge_maker('PAULEEN')
+batch_badge_creator('welcome back!')
